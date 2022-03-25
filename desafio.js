c56e36c25657e81f5d7a1051ffacb1d0c14c1013
Emiliano <bocaemiliano@hotmail.com>
2022-03-25 16:21:35 -0300

    desafios-js

--- desafio.js
+++ desafio.js
@@ -1,51 +1,73 @@
-/* ejercicio edad de messi*/
-
-let i = prompt("ingrese la edad de Messi");
-  while ( i  != "34"){ 
-        
-     i = prompt(i + " No es la edad de Messi");
-       
-  } 
-  alert("Felicitaciones! " + i 
-  + " es la edad de Messi");
-
-
-/* listado de nombres en pantalla*/
-
-  let nombres = "";
-
-for (i = 0; i <5 ; i ++){
-  
-     nombres += prompt("ingrese 5 nombres ")+"\n";
-}
-alert("los nombres ingresados son: " + nombres); 
 
 
 
 
-
-/* ejemplos de IF */
-
-let usuario = prompt("Ingrese su nombre");
-
-alert("Hola : " + usuario);
-
-let edad = prompt ("¿Cuantos años tienes?");
+                                  
 
 
-if (edad < 18){
-     alert("Eres menor de edad porque tienes " + edad + " años");
-}
-else if ( (edad >= 18) && (edad < 30) )    {
-    alert("Eres mayor de edad");
+function CantResultado(){
+
+let cantidad = prompt("Ingrese la cantidad de carteras que va a comprar");
+
+alert ("Usted compro " + cantidad + " carteras");
 } 
+ CantResultado ();
 
-else if ( (edad >= 30) && (edad <= 50) )  {
-      alert("eres adulto");
-}
 
-else { 
-    alert("eres viejo");
-}
+let color = prompt("Ingrese el color de la cartera que va a comprar");
+
+  while ( (color != "rojo") && (color != "negro") && (color != "blanco") && (color != "azul") ) {
+           
+     color = prompt ("No tenemos de ese color");
+ }
+   alert("Usted elijio el color " + color );
+
+   
+   const porcentaje = (monto) => monto * 20/100
+    monto = prompt ("ingrese el monto de la cartera para obtener el %20 de descuento");
+   let resultado = monto - porcentaje (monto);
+   alert ("Su descuento es de: $" + porcentaje (monto) + ", el total seria: $"+ resultado);  
+
+
+
+   let dato = prompt("Elija una opcion para pagar en cuotas:  \n 1 -  2/cuotas \n 2 -  3/cuotas \n 3 -  6/cuotas ");
+ 
+   if (dato == "1"){
+      alert("Elejiste pagar en 2 cuotas");
+       let dato = prompt ("ingrese el total del monto para saber el valor de las cuotas");
+       let montocuotas = dato / 2;
+           alert("pagara " + montocuotas + " en dos meses.");
+   
+   }
+      else if (dato == "2"){
+           alert("Elejiste pagar en 3 cuotas");
+           let dato = prompt ("ingrese el total del monto para saber el valor de las cuotas");
+           let montocuotas = dato / 3;
+             alert("pagara " + montocuotas + " en tres meses.");
+
+      }
+
+      else if (dato == "3"){
+           alert("Elejiste pagar en 6 cuotas");
+           let dato = prompt ("ingrese el total del monto para saber el valor de las cuotas");
+           let montocuotas = dato / 6;
+             alert("pagara " + montocuotas + " en 6 meses.");
+      }
+
+      else {
+            alert("no existe esa opcion");
+      }
+
+      
+
+
+
+
+
+   
+
+
+
+   
 
 
