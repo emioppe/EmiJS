3f5635d1b686bbdd52ba5f008b61822535aae256
Emiliano <bocaemiliano@hotmail.com>
2022-04-04 18:01:22 -0300

    desafios-js

--- desafio.js
+++ desafio.js
@@ -1,17 +1,17 @@
 
 /* PRE ENTREGA N1 ARRAYS, METODOS DE BUSQUEDA Y FILTRADO SOBRE EL ARRAY*/ 
 
-const Carteras = ["CArteraflecos", "Carteratiras", "Carteraestrella", "Bandoleracombinada"];
+const Carteras = ["Carteraflecos", "Carteratiras", "Carteraestrella", "Bandoleracombinada"];
        Carteras.push (Carteras);
            console.log(Carteras);
 
 
 
 
-           const productos = ["Cartera","Bolsos","Mochilas","Accesorios"];
+           const Productos = ["Cartera","Bolsos","Mochilas","Accesorios"];
 
-           for (let i= 0; i < productos.length; i++) {
-               console.log(productos[i]);
+           for (let i= 0; i < Productos.length; i++) {
+               console.log(Productos[i]);
            }
            
 
